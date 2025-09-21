b7cb059b3bf250f4519eac8649deee4c320b59c1
ayaan-gupta1644 <ayaangupta79080@gmail.com>
2025-09-21 11:34:22 +0530

    first

--- installing.py
+++ installing.py
@@ -1,9 +1,1 @@
-from fastapi import FastAPI
-
-# Create an instance of the FastAPI class
-app = FastAPI()
-
-# Define a simple GET endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+print("hello world ")
